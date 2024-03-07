4aeb79b7d53c14d54deec68aa80b85f0abfb815a
21Alul21 <augustine.alul@yahoo.com>
2024-03-07 16:55:41 +0100

    added the extra challenge

--- day_8.py
+++ day_8.py
@@ -25,8 +25,35 @@ def odd_even(number_list: List[int]) -> int:
   
     return max_even - min_odd
 
+
+# Extra Challenge: List of Prime numbers
+from typing import List
+
+
+def prime_numbers() -> List[int]:
+    """ function that asks the user for
+    input and returns a list of prime numbers
+    within its range
+    """
+    prime_list = list()
+    try:
+        value = int(input("enter a number: "))
+        for i in range(0, value + 1):   # looping through the elements in the value/input range
+            if i > 1:
+                for j in range(2, i):
+                    if i % j == 0:
+                        break
+                    else:
+                        prime_list.append(i)
+        return prime_list
+                    
+
+    except ValueError as e:
+        return f"please enter a number, {e}"
+
     
 if __name__ == "__main__":
       print(odd_even([2,3,4,5,6,7])) # outputs 3
+      print(prime_numbers())
 
 
