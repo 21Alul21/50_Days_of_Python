d98eddba052d32ad0724a624f053fde738c7ca7b
21Alul21 <augustine.alul@yahoo.com>
2024-03-14 21:31:15 +0100

    day 11 extra task done

--- day_11.py
+++ day_11.py
@@ -20,6 +20,24 @@ def equal_strings(str1: str, str2: str) -> bool:
                     
         
     return False
+
+# Extra Challenge: Swap Values
+from typing import List
+def swap_values(list_arg: List[int]) -> List[int]:
+    """" function that takes list of numbers and
+    swaps the first element with the last element
+    """
+
+    first_int = list_arg[0]
+    last_int = list_arg[-1]
+    list_arg.pop(0)
+    list_arg.pop(-1)
+    list_arg.insert(0, last_int)
+    list_arg.append(first_int)
+    
+    
+    return list_arg
                 
 if __name__ == '__main__':
-    print(equal_strings('live', 'vole'))
+    print(equal_strings('love', 'volt'))    # outoputs false
+    print(swap_values([1,23,5,6]))          # outputs [6, 23, 5, 1]
