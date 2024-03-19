43a8e779680cf0b5269bd371cf8f46a627448e16
21Alul21 <augustine.alul@yahoo.com>
2024-03-19 15:22:04 +0100

    added the code for extra challenge

--- day_12.py
+++ day_12.py
@@ -16,6 +16,29 @@ def count_dots(dotted_string: str) -> str:
     return f'{count} dots'
 
 
+#Extra Challenge: Your Age in minutes
+from datetime import datetime
+
+def age_in_minutes():
+    while True:
+        try:
+            birth_year = input('enter your birth year: ')
+            if len(birth_year) != 4:
+                print('invalid input, enter 4 digits for birth year')
+            if int(birth_year) < 1900:
+                print('your input is invalid, enter a birth year that is \
+                        greater than or equal to 1900')
+            if int(birth_year) > int(datetime.now().strftime('%Y')):
+                print('input a valid year, you are ahead of time')
+            else:
+                age = (int(datetime.now().strftime('%Y'))) - int(birth_year) 
+                return f' You are {age * 525600} mins old'
+                break
+        except Exception as e:
+            print(f'erorr: {e}')
+
 
 if __name__ == '__main__':
-    print(count_dots('a..s'))   # outputs 
+    print(count_dots('a..s'))   # outputs 2
+    print(age_in_minutes())     # outputs your age in mins
+
