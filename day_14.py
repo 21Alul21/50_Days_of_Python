b1505c7318032cc1629ffb3a56f5e470e63b523a
21Alul21 <augustine.alul@yahoo.com>
2024-03-25 23:45:42 +0100

    added solution for the extra task

--- day_14.py
+++ day_14.py
@@ -8,10 +8,30 @@ def flat_list(n_list: List[List]) -> List[int]:
     """ function that takes a nested list and flatten
     it to a 1D list
     """
-    # flat_list = list(flat_list)
-    # first = flat_list.pop(0)
-    # last = flat_list.pop[-1]
+  
     return n_list[0]
+
+
+
+# Extra Challenge: Teacher's Salary
+def your_salary():
+    name_teacher = input('enter the teacher\'s name: ' )
+    period_number = int(input('number of periods taught in a month: '))
+    rate = float(input('enter rate per period: '))
+    if period_number > 100:
+        extra_time = period_number - 100
+        extra_time_sum = extra_time * 25
+        gross = (100 * 20) + extra_time_sum
+        return f'Teacher: {name_teacher},\n Period: {period_number}\n\
+            Gross salary: {gross}'
+    else:
+        gross = period_number * 20
+        return f'Teacher: {name_teacher},\n Period: {period_number}\n\
+        Gross salary: {gross}'
+
+
 
 if __name__ == '__main__':
     print(flat_list([[1,2,3,4]]))
+
+    print(your_salary())
